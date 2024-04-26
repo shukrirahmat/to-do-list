4c94e296c5949e2be8884041c207b4c205beef55
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-04-27 00:23:26 +0800

    add basic structure of the html

---
+++ src/todo.js
@@ -0,0 +1,8 @@
+function createToDo(title, description, dueDate, priority) {
+    const note = "";
+    const checklist = false;
+
+    return {title, description, dueDate, priority, note, checklist};
+}
+
+export {createToDo};
